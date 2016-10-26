4f423c4c3e9b5b8ee65ebe62342b4e100597d798
aadhithya <aadhi95balaji@gmail.om>
2016-10-27 03:13:47 +0530

    added extra array to contain the fit chromosomes added excel support

--- ga.py
+++ ga.py
@@ -1,16 +1,18 @@
 import random
 import statistics as sta
+import xlwt
 
 class generation:
     def __init__(self):
         ## INITIALISATION VARIABLES
         self.fit=100
         self.totstu=1000 ## TOTAL NUMBER OF STUDENTS
-        self.stuperroom=4 ## STUDENTS PER ROOM
+        self.stuperroom=5 ## STUDENTS PER ROOM
         self.stulist=[] ## MAIN ARRAY THAT STORES THE STUDENTS CHOICES
         self.li=[] ## USED FOR RANDOMLY GENERATING CHROMOSOMES
         self.chromelist=[]  ## CHROMOSOME CONTAINER FOR THE GENERATION
         self.optrang=[4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4] ## RANGE OF CHOICES AVAILABLE TO EACH ATTRIBUTE LENGTH OF THIS LIST DECIDES THE NUMBER OF ATTRIBUTES PER STUDENT
+        self.mainlist=[]
 
     ## THE CHROMOSOME CLASS
     class chromo:
@@ -44,8 +46,8 @@ class generation:
         for val in self.chromelist:
             val.calc_fitness(self.stulist)
             sum=sum+val.fitness
-        self.fitness=sum/len(self.chromelist)
-        print(self.fitness)
+        self.fit=sum/len(self.chromelist)
+        ##print(self.fit)
         
     ## FUNCTION FOR GENERATING THE INITIAL CHROMOSOMES
     def setinitstate(self):
@@ -68,15 +70,15 @@ class generation:
     def crossover(self):
         chlist1=list(self.chromelist)
         chlist=[]
+        count=0
         chlist1.sort(key=lambda x: x.fitness, reverse=False)
         for ch in chlist1:
-            if ch.fitness < 1.15:
-                chlist.append(ch)
+            if ch.fitness < 1:
+                count+=1
+                self.mainlist.append(ch)
                 chlist1.remove(ch)
-            else:
-                break
         prev=0
-        print(len(chlist1))
+        print(len(self.mainlist))
         for ch in chlist1:
             ch.fitness=((ch.fitness-chlist1[0].fitness)/(chlist1[len(chlist1)-1].fitness-chlist1[0].fitness))
         while len(chlist1)>0:
@@ -117,13 +119,41 @@ class generation:
 g1=generation()
 g1.generate_data()
 g1.setinitstate()
+wb = xlwt.Workbook()
+wb.add_sheet("init")
+c_sh=wb.get_sheet(0)
+for m in range(len(g1.mainlist)):
+    for n in range(g1.stuperroom):
+        c_sh.write(m, n, g1.mainlist[m].p[n])
+    c_sh.write(m, g1.stuperroom + 1, str(g1.mainlist[m].fitness))
+for m in range(len(g1.chromelist)):
+    for n in range(g1.stuperroom):
+        c_sh.write(len(g1.mainlist)+m, n, g1.chromelist[m].p[n])
+    c_sh.write(len(g1.mainlist)+m, g1.stuperroom + 1, str(g1.chromelist[m].fitness))
+c_sh.write(len(g1.chromelist)+1,0,str(g1.fit))
+sheetno=1
+for j in range(10):
+    for i in range(10):
+        g2 = generation()
+        a = g1.crossover()
+        g2.stulist = list(g1.stulist)
+        g2.chromelist = a
+        g2.mainlist=g1.mainlist
+        g2.calcu_fitness()
+        g1 = g2
+        wb.add_sheet("Generation "+str(j)+","+str(i))
+        c_sh = wb.get_sheet(sheetno)
+        sheetno=sheetno+1
+        for m in range(len(g2.mainlist)):
+            for n in range(g2.stuperroom):
+                c_sh.write(m,n,g2.mainlist[m].p[n])
+            c_sh.write(m,g1.stuperroom+1,str(g2.mainlist[m].fitness))
+        for m in range(len(g2.chromelist)):
+            for n in range(g2.stuperroom):
+                c_sh.write(len(g2.mainlist)+m, n, g2.chromelist[m].p[n])
+            c_sh.write(len(g2.mainlist)+m, g1.stuperroom + 1, str(g2.chromelist[m].fitness))
+        ##print(g2.fit)
+        c_sh.write(len(g2.mainlist)+len(g2.chromelist)+1,0,str(g2.fit))
+wb.save("ga.xls")
 
-for i in range(1000):
-    a=g1.crossover()
-    g2=generation()
-    g2.stulist=list(g1.stulist)
-    g2.chromelist=a
-    g2.calcu_fitness()
-    g1=g2
-    
 
